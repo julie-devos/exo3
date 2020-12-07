56ee6388773df69381941b4feb7dc72a6ea3f5a0
julie-devos <julie.devos@epitech.eu>
2020-12-07 15:04:08 +0100

    commit

---
+++ exo_3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>PhpPart1Exo3</title>
+</head>
+<body>
+	<?php
+		$km=1;
+	 ?>
+	 <p><?=$km?></p><br>
+	 <?php
+ 		$km=3;
+ 	 ?>
+	 <p><?=$km?></p><br>
+	 <?php
+ 		$km=125;
+ 	 ?>
+	 <p><?=$km?></p><br>
+</body>
+</html>
